d8e999ccaa3b5aa68cb50e3d3cdebb17dd12d235
MertOzmen <avmertozmen93@gmail.com>
2024-05-23 23:53:30 +0300

    username and password locators have been found.

--- src/test/java/Pages/DialogContent_US_001.java
+++ src/test/java/Pages/DialogContent_US_001.java
@@ -2,8 +2,17 @@ package Pages;
 
 import Utilities.GWD;
 import Utilities.ParentPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DialogContent_US_001 extends ParentPage {
     public DialogContent_US_001() {PageFactory.initElements(GWD.getDriver(),this);}
+
+    @FindBy(xpath = "(//div[@class='mdc-notched-outline__notch'])[1]")
+    public WebElement username;
+
+    @FindBy(css = "[class='mdc-notched-outline ng-tns-c1534922977-50 mdc-notched-outline--upgraded ng-star-inserted']")
+    public WebElement password;
+
 }
