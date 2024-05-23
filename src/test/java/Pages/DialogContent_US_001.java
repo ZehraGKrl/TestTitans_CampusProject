9b9f25a3bf3abd8d6a6f30f56e8373e702d60544
MertOzmen <avmertozmen93@gmail.com>
2024-05-23 23:48:55 +0300

    Dialog content class environment has been created for finding Locators.

--- src/test/java/Pages/DialogContent_US_001.java
+++ src/test/java/Pages/DialogContent_US_001.java
@@ -1,4 +1,9 @@
 package Pages;
 
-public class DialogContent_US_001 {
+import Utilities.GWD;
+import Utilities.ParentPage;
+import org.openqa.selenium.support.PageFactory;
+
+public class DialogContent_US_001 extends ParentPage {
+    public DialogContent_US_001() {PageFactory.initElements(GWD.getDriver(),this);}
 }
