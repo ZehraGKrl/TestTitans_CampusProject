34074908ba55bbeb8a27202fe2f88679c4b56c59
Degisiklikler1 <megaozi9517536@gmail.com>
2024-05-23 15:15:04 +0300

    LeftNav_US_3 locater'ları bulunmaya başlandı.

--- src/test/java/Pages/DialogContent_US_3.java
+++ src/test/java/Pages/DialogContent_US_3.java
@@ -1,4 +1,15 @@
 package Pages;
 
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class DialogContent_US_3 {
+
+    public DialogContent_US_3() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+
 }
--- src/test/java/Pages/LeftNav_US_3.java
+++ src/test/java/Pages/LeftNav_US_3.java
@@ -1,4 +1,39 @@
 package Pages;
 
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LeftNav_US_3 {
+
+    public LeftNav_US_3() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(css = "[id='mat-input-2']")
+    public WebElement username;
+    @FindBy(id = "mat-input-3")
+    public WebElement password;
+    @FindBy(css = "[class='mdc-button__label']")
+    public WebElement loginButton;
+
+
+    public WebElement getWebelement(String element){
+
+        switch (element){
+
+            case "username":
+                return this.username;
+            case "password":
+                return this.password;
+            case "loginButton":
+                return this.loginButton;
+
+        }
+        return null;
+    }
+
+
+
 }
