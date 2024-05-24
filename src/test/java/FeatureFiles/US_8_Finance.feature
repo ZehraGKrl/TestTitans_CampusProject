7eec8e382f7e74bd1fb2a74ddf6e68b5adc7033c
Degisiklikler1 <megaozi9517536@gmail.com>
2024-05-24 21:35:24 +0300

    değişiklikler yapıldı

---
+++ src/test/java/FeatureFiles/US_8_Finance.feature
@@ -0,0 +1,8 @@
+Feature: Finance Functionality
+
+  Background:
+    Given Navigate to Campus
+
+   Scenario: Making Payments in The Finance Section
+     Given Enter username and password and click login button
+     When Make payment by clicking on the finance section
---
+++ src/test/java/StepDefinitions/HamburgerMenu/US_8_FinanceSteps.java
@@ -0,0 +1,17 @@
+package StepDefinitions.HamburgerMenu;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class US_8_FinanceSteps {
+    @Given("Enter username and password and click login button")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+
+    }
+
+    @When("Make payment by clicking on the finance section")
+    public void makePaymentByClickingOnTheFinanceSection() {
+
+
+    }
+}
