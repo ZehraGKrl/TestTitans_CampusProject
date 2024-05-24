71b3d0e9b9aae5346c20e7225a736502f250e410
MertOzmen <avmertozmen93@gmail.com>
2024-05-25 02:09:11 +0300

    Click on the element in Top Menu step have been added

--- src/test/java/StepDefinitions/HamburgerMenu/US_006_Messaging.java
+++ src/test/java/StepDefinitions/HamburgerMenu/US_006_Messaging.java
@@ -1,10 +1,14 @@
 package StepDefinitions.HamburgerMenu;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
+
+import java.util.List;
 
 public class US_006_Messaging {
     @And("Click on the element Top Menu")
-    public void clickOnTheElementTopMenu() {
+    public void clickOnTheElementTopMenu(DataTable text) {
+        List<String> textList = text.asList(String.class);
 
     }
 }
