8dc8cc16227292c3fe89e8676cfac98149c17b07
elcnburak <elcnburak@hotmail.com>
2024-05-25 12:05:12 +0100

    US-5-10-15-20-25 created feature files and stepdefinitons files

---
+++ src/test/java/FeatureFiles/US_025_Calendar.feature
@@ -0,0 +1,18 @@
+Feature: Calendar Functionality
+  Background:
+    Given  Navigate to Campus
+    When User sending the keys in Dialog
+      | usernameInput | Student_7 |
+      | passwordInput | S12345    |
+    Then Click on the element in Dialog
+      | loginButton Assignment Functionality|
+    And Main page should be displayed
+
+    Scenario: Calendar Functionality
+      When the user clicks the completed lesson and recording button
+      Then the user see the lesson video
+      And the user clicks the lesson play button
+
+
+
+
---
+++ src/test/java/StepDefinitions/HamburgerMenu/US_020_Assignments.java
@@ -0,0 +1,23 @@
+package StepDefinitions.HamburgerMenu;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_020_Assignments {
+    @When("the user clicks the assignments button")
+    public void theUserClicksTheAssignmentsButton() {
+    }
+
+    @Then("the user selects a homework and clicks the information button, submit button, and favorite button")
+    public void theUserSelectsAHomeworkAndClicksTheInformationButtonSubmitButtonAndFavoriteButton() {
+    }
+
+    @And("the user selects and clicks a homework and sees the homework information")
+    public void theUserSelectsAndClicksAHomeworkAndSeesTheHomeworkInformation() {
+    }
+
+    @And("the user clicks the discuss button")
+    public void theUserClicksTheDiscussButton() {
+    }
+}
