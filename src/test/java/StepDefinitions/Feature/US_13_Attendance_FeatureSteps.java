b0b269e2758b4744ad54ba56d095a3f28af51bc1
Degisiklikler1 <megaozi9517536@gmail.com>
2024-05-27 18:51:45 +0300

    US_13 OLUŞTURULDU

---
+++ src/test/java/StepDefinitions/Feature/US_13_Attendance_FeatureSteps.java
@@ -0,0 +1,16 @@
+package StepDefinitions.Feature;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class US_13_Attendance_FeatureSteps {
+    @Given("Click on the attendance button and attendance excuse button")
+    public void clickOnTheAttendanceButtonAndAttendanceExcuseButton() {
+
+    }
+
+    @When("Enter an excuse")
+    public void enterAnExcuse() {
+
+    }
+}
