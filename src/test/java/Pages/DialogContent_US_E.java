1fa49f3eca0f6fe49c5a4d66d7ca23bfcee2a889
elcnburak <elcnburak@hotmail.com>
2024-05-29 18:56:36 +0100

    us-25 completed

--- src/test/java/Pages/DialogContent_US_E.java
+++ src/test/java/Pages/DialogContent_US_E.java
@@ -11,6 +11,7 @@ public class DialogContent_US_E extends ParentPage {
     public DialogContent_US_E() {
         PageFactory.initElements(GWD.getDriver(), this);
     }
+
     @FindBy(xpath = "//span[contains(@class, 'username') and contains(@class, 'mr-12')]")
     public WebElement profileButton;
 
@@ -29,6 +30,20 @@ public class DialogContent_US_E extends ParentPage {
     @FindBy(xpath = "//div[text()='Profile successfully updated']")
     public WebElement successMessage;
 
+    @FindBy(xpath = "//span[text()='Calendar']")
+    public WebElement calendar;
+
+    @FindBy(xpath = "//span[@class='mat-mdc-focus-indicator']")
+    public WebElement previousButton;
+
+    @FindBy(xpath = "//*[@id=\"mat-tab-content-0-0\"]/div[1]/ms-course-schedule-board[1]/ms-browse[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[6]/div[2]/div[1]/div[1]/div[1]/span[1]")
+
+    public WebElement lesson;
+
+    @FindBy(xpath = "//span[@class='ng-star-inserted' and @style='visibility: visible;' and text()='Recording']")
+    public WebElement recordingButton;
+
+
     public WebElement getWebElement(String strElement) {
         switch (strElement) {
             case "profileButton":
@@ -43,8 +58,14 @@ public class DialogContent_US_E extends ParentPage {
                 return this.saveButton;
             case "isSuccessMessageDisplayed":
                 return this.successMessage;
+            case "calendar":
+                return this.calendar;
+            case "previousButton":
+                return this.previousButton;
 
         }
-        return null;
+            return null;
+        }
+
     }
-}
+
--- src/test/java/StepDefinitions/Calendar/US_025_Calendar.java
+++ src/test/java/StepDefinitions/Calendar/US_025_Calendar.java
@@ -1,19 +1,79 @@
 package StepDefinitions.Calendar;
-
+import Pages.DialogContent_US_E;
+import Utilities.ParentPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class US_025_Calendar {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import static Utilities.GWD.getDriver;
+
+public class US_025_Calendar extends ParentPage {
+    DialogContent_US_E dc = new DialogContent_US_E();
+
     @When("the user clicks the completed lesson and recording button")
-    public void theUserClicksTheCompletedLessonAndRecordingButton() {
+    public void theUserClicksTheCompletedLessonAndRecordingButton() throws InterruptedException, AWTException {
+
+        dc.myClick(dc.calendar);
+        Thread.sleep(5000);
+
+        Robot robot = new Robot();
+
+        for (int i = 0; i < 12; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        }
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+        WebDriver driver = getDriver();
+        Duration timeout = Duration.ofSeconds(80);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+
+
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[text()=' Friday, May 24 ']")));
+
+        if (element.isDisplayed()) {
+            System.out.println("Element görünür.");
+        } else {
+            System.out.println("Element görünür değil.");
+        }
+        WebElement lessonElement = wait.until(ExpectedConditions.elementToBeClickable(dc.lesson));
+        lessonElement.click();
+
+        dc.myClick(dc.recordingButton);
+
+        Thread.sleep(10000);
+
     }
 
-    @Then("the user see the lesson video")
-    public void theUserSeeTheLessonVideo() {
-    }
+    @Then("the user see the lesson video and play button")
+    public void theUserSeeTheLessonVideoAndPlayButton() throws AWTException {
 
-    @And("the user clicks the lesson play button")
-    public void theUserClicksTheLessonPlayButton() {
+        try {
+
+            Robot robot = new Robot();
+
+            for (int i = 0; i < 6; i++) {
+                robot.keyPress(KeyEvent.VK_TAB);
+                robot.keyRelease(KeyEvent.VK_TAB);
+                Thread.sleep(100);
+            }
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
+        } catch (AWTException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
