f28dba25e8bf793f9400c3816e42bbd3967e7513
elcnburak <elcnburak@hotmail.com>
2024-05-30 23:09:49 +0100

    us-10 completed

--- src/test/java/FeatureFiles/US_010_Hamburger_Finance.feature
+++ src/test/java/FeatureFiles/US_010_Hamburger_Finance.feature
@@ -10,12 +10,10 @@ Feature: Finance Functionality
 
   Scenario: Finance Functionality
     When the user clicks the finance button and the my finance button
-    And the user clicks the name
-    And the user sees the online payment button and the Fee_Balance Detail button
     And the user selects the Stripe button and clicks the pay button
     And the user clicks the Fee_Balance Detail button and sees the payment
-    And the user sees all payments and balances or debts
-    Then the user should see the message at the top right
+    Then the user sees all payments and balances or debts
+
 
 
 
--- src/test/java/Pages/DialogContent_US_E.java
+++ src/test/java/Pages/DialogContent_US_E.java
@@ -37,35 +37,56 @@ public class DialogContent_US_E extends ParentPage {
     public WebElement previousButton;
 
     @FindBy(xpath = "//*[@id=\"mat-tab-content-0-0\"]/div[1]/ms-course-schedule-board[1]/ms-browse[1]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[6]/div[2]/div[1]/div[1]/div[1]/span[1]")
-
     public WebElement lesson;
 
     @FindBy(xpath = "//span[@class='ng-star-inserted' and @style='visibility: visible;' and text()='Recording']")
     public WebElement recordingButton;
 
+    @FindBy(xpath = "//td[contains(@class, 'cdk-column-createdDate') and text()='05/27/2024']")
+    public WebElement studentName;
 
-    public WebElement getWebElement(String strElement) {
-        switch (strElement) {
-            case "profileButton":
-                return this.profileButton;
-            case "settingsButton":
-                return this.settingsButton;
-            case "purpleTheme":
-                return this.purpleTheme;
-            case "defaultTheme":
-                return this.defaultTheme;
-            case "saveButton":
-                return this.saveButton;
-            case "isSuccessMessageDisplayed":
-                return this.successMessage;
-            case "calendar":
-                return this.calendar;
-            case "previousButton":
-                return this.previousButton;
+    @FindBy(xpath = "//label[@class='mdc-label']")
+    public WebElement stripeButton;
 
-        }
-            return null;
-        }
+    @FindBy(css = "[class='w-50-p discount-class']")
+    public WebElement allPayment;
 
-    }
+    @FindBy(xpath = "(//span[@class='w-50-p'])[2]")
+    public WebElement payButton;
+
+    @FindBy(xpath = "//ms-currency-field[@formcontrolname='customAmountField']//input")
+    public WebElement amountButton;
+
+    @FindBy(xpath = "(//span[@class='ng-star-inserted'])[9]")
+    public WebElement paymentButton;
+
+    @FindBy(css = "[class='__PrivateStripeElement']>iframe")
+    public WebElement iframe;
+
+    @FindBy(css = "[name='number']")
+    public WebElement cardNumberButton;
+
+    @FindBy(css = "[id='Field-expiryInput']")
+    public WebElement expirationButton;
+
+    @FindBy(css = "[id='Field-cvcInput']")
+    public WebElement cvcButton;
+
+    @FindBy(xpath = "((//span[@class='mdc-button__label'])[15]//following-sibling::span)[1]")
+    public WebElement stripePaymentsButton;
+
+    @FindBy(css = "[class='mat-expansion-panel-header-description ng-star-inserted']")
+    public WebElement paymentSuccessMessage;
+
+    @FindBy(xpath = "//span[text()='Fee/Balance Detail']\n")
+    public WebElement feeBalanceDetail;
+
+    @FindBy(css = "html > body > app > student-layout > div > student-toolbar-horizontal > mat-toolbar > div > div:nth-of-type(2) > user-message-bell > button > span:nth-of-type(2)\n")
+    public WebElement messageButton;
+
+    @FindBy(xpath = "//td[contains(text(), 'Thu May 30, 2024 22:51')]\n")
+    public WebElement firstMessage;
+
+
+}
 
