d50cdd7ded7900c7ea3f098e4948991ab2eb715c
MertOzmen <avmertozmen93@gmail.com>
2024-07-09 12:56:31 +0300

    Requirements updated

--- src/test/java/StepDefinitions/HamburgerMenu/US_004_MessagingSteps.java
+++ src/test/java/StepDefinitions/HamburgerMenu/US_004_MessagingSteps.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.Then;
 public class US_004_MessagingSteps {
     @Then("User should be able to see related buttons")
     public void userShouldBeAbleToSeeRelatedButtons() {
+
     }
 }
